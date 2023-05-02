b49dbd9857d1c9b3efed1f08a95e4ee00ed926ac
Cedric <samcedric490@gmail.com>
2023-05-02 12:59:58 +0700

    Remove unused view

--- utils/views.py
+++ utils/views.py
@@ -1,6 +1,6 @@
 from miru.ext import nav
 from utils.genshin_impact import get_weapon_info, GenshinWeapon, WeaponButtonType
-from utils import get_syntax, PartialCommand, Colour
+from utils import Colour
 
 
 import hikari
@@ -9,7 +9,7 @@ import typing as t
 import lightbulb
 
 
-__all__ = ('HelpView', 'Paginator', 'WeaponPaginator', 'DailySetupView')
+__all__ = ('Paginator', 'WeaponPaginator', 'DailySetupView')
 
 
 class WeaponPaginator(miru.View):
@@ -135,43 +135,6 @@ class WeaponPaginator(miru.View):
 
     async def on_timeout(self) -> None:
         await self.message.edit(components=None)
-
-
-class HelpView(nav.NavigatorView):
-
-    def __init__(self, source, title, color, page, timeout=30, footer="{page}"):
-        self.source: t.Union[list, tuple] = source
-        self.title: str = title
-        self.color: str = color
-        self.page: int = page
-        self.footer: str = footer
-        self.embeds = self._format_page()
-
-        super().__init__(timeout=timeout, pages=self.embeds)
-
-    def _build_embed(self, data: t.List[PartialCommand], footer: str) -> hikari.Embed:
-        embed = hikari.Embed(color=self.color, title=self.title)
-        embed.set_footer(text=footer)
-
-        fields = [(command.name.title(), f'{command.description}\n`{get_syntax(command)}`', False) for command in data]
-
-        for name, value, inline in fields:
-            embed.add_field(name=name, value=value, inline=inline)
-
-        return embed
-
-    def _format_page(self) -> t.List[hikari.Embed]:
-        divided_source = [self.source[i:i + self.page] for i in range(0, len(self.source), self.page)]
-        result_embeds = []
-
-        for idx, x in enumerate(divided_source, 1):
-            embed = self._build_embed(
-                x, footer=f"Page {idx} of {len(divided_source)}"
-                if self.footer == '{page}' else self.footer
-            )
-            result_embeds.append(embed)
-
-        return result_embeds
 
 
 class Paginator(nav.NavigatorView):
